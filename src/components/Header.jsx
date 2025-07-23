bef4a470504734a71f3383a72ed2909b10025556
Balaji Bandgar <balajibandgar26@gmail.com>
2025-07-23 13:30:04 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
-import { Sun, Moon, Code } from 'lucide-react';
+import { Sun, Moon } from 'lucide-react';
 
 function Header({ theme, onToggleTheme }) {
   const isDark = theme === 'vs-dark';
   
   return (
-    <header className="bg-gray-800 p-4 border-b border-gray-600 flex items-center justify-between">
-      <div className="flex items-center space-x-2">
-        <Code size={24} />
+    <header className="bg-gray-800 p-4 border-b border-gray-600 flex items-center justify-between" style={{padding:5}}>
+      <div className="flex items-center">
+        <img src="/vs-code.svg" alt="logo" className='h-8'/>
         <h1 className="text-xl font-bold">VSCode Lite</h1>
       </div>
       
       <button
         onClick={onToggleTheme}
-        className="p-2 hover:bg-gray-700 rounded-md transition-colors"
+        className="bg-gray-700 h-10 w-10 rounded-md transition-colors flex items-center justify-center"
         title={isDark ? 'Switch to Light Theme' : 'Switch to Dark Theme'}
       >
-        {isDark ? <Sun size={20} /> : <Moon size={20} />}
+        {isDark ? <Sun size={20} className='text-yellow-200'/> : <Moon size={20} className='text-blue-300'/>}
       </button>
     </header>
   );
